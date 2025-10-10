4dfb0634fb35059832b25debd56fa051dbf09001
Ryan Jackson <ryan@ryanmjackson.com>
2025-10-10 03:28:05 -0500

    Update happy-coder.rb

--- Formula/happy-coder.rb
+++ Formula/happy-coder.rb
@@ -3,8 +3,8 @@ class HappyCoder < Formula
   homepage "https://github.com/slopus/happy-cli"
 
   # Choose ONE url/sha pair (I kept the newer one here)
-  url "https://github.com/slopus/happy-cli/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "9d6245736f121763f8d2b0dc524f774f58d2a1f00254ebd7c0131beebdd16db1"
+  url "https://github.com/slopus/happy-cli/archive/refs/tags/v0.11.2-0.tar.gz"
+  sha256 "c0e27ebe291f24a5a61c5939c8d7eefa555cf06a1d7a4cd8220513af52e74c28"
 
   license "MIT"
 
